5d8bbd617d3d25c583208d7ceca33eda34d804d4
purreB <jakob.bergstrom@live.se>
2022-02-03 17:01:44 +0100

    Added interfaces

---
+++ Natur_Ecommerce/src/models/Cart.ts
@@ -0,0 +1,5 @@
+import { Product } from './Products';
+
+export interface Cart {
+  products: [Product];
+}
---
+++ Natur_Ecommerce/src/models/Products.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  name: string;
+  price: number;
+}
+
+export interface ProductList {
+  products: [Product];
+}
