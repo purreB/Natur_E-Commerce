f3cca974d544b729a886cb316322aa037a51e4e3
purreB <jakob.bergstrom@live.se>
2022-02-03 15:09:20 +0100

    Removed React import in app test

--- Natur_Ecommerce/src/App.test.tsx
+++ Natur_Ecommerce/src/App.test.tsx
@@ -1,6 +1,5 @@
 import App from './App';
 import { render } from '@testing-library/react';
-import React from 'react';
 
 describe('App testing', () => {
   it('should render without crashing', () => {
