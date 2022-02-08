c9b77b0c94290c95f2fcb231e42d6e2434a6e3d6
purreB <jakob.bergstrom@live.se>
2022-02-08 12:02:14 +0100

    Added ids to products

--- Natur_Ecommerce/src/components/SearchBar/SearchBar.tsx
+++ Natur_Ecommerce/src/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function SearchBar() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>This is searchbar</h1>
+    </div>
+  );
 }
 
 export default SearchBar;
